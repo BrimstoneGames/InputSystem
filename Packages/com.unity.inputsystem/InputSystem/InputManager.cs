77272b6e2c63413e8e46595138ff3c4029e4816a
Dmytro Ivanov <dmytro@unity3d.com>
2020-09-11 15:45:22 +0200

    Partial refactoring (WIP)

--- Packages/com.unity.inputsystem/InputSystem/InputManager.cs
+++ Packages/com.unity.inputsystem/InputSystem/InputManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using InputSystem.OldInputCompatibility;
 using Unity.Collections;
 using UnityEngine.InputSystem.Composites;
 using UnityEngine.InputSystem.Controls;
